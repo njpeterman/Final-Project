1c6a20dbd025fd5e97fd8f4f67ee067cf53f1158
njpeterman <njpeterman@gmail.com>
2015-11-11 00:24:22 -0600

    Defined global variables for time step, boundaries

--- logic.h
+++ logic.h
@@ -1,7 +1,14 @@
 #ifndef LOGIC_H_
 #define LOGIC_H_
 
-#define GRAVITY 5
+#define GRAVITY 9.81
+#define IMPULSE 30
+#define IMPULSE_TIME_LIMIT 1000
+#define JUMP_STEP 5
+#define TIME_STEP 0.1
+#define RIGHT_BOUND 639
+#define BOTTOM_BOUND 439
+
 
 // insert functions implemented here
 
